a1a75242a94c887149c870f48c570d05233c02ab
Junmo <hongjm1022@gmail.com>
2025-09-21 02:02:01 +0900

    site update

--- public/README.txt
+++ public/README.txt
@@ -1,4 +1,6 @@
 Place your APK file here as `siupay-latest.apk` (or update APK_DOWNLOAD_URL in src/constants.js)
 
+Place your logo image here as `siupay-mark.png` to replace the navbar/footer logo.
+
 
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,7 @@ export default function Footer() {
       <div className="container">
         <div className="footer-content">
           <div className="footer-logo">
-            <img src={`data:image/svg+xml,${encodeURIComponent(`
-              <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32'>
-                <path fill='#4BA3FF' d='M16 2C8.268 2 2 8.268 2 16s6.268 14 14 14 14-6.268 14-14S23.732 2 16 2zm0 25c-6.065 0-11-4.935-11-11S9.935 5 16 5s11 4.935 11 11-4.935 11-11 11z'/>
-                <path fill='#4BA3FF' d='M16 8c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8zm0 13c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z'/>
-              </svg>
-            `)}`} alt="SIUPAY" />
+            <img src="https://github.com/user-attachments/assets/5d995edd-0690-4c55-b113-70797368bd9f" alt="SIUPAY" width="32" height="32" />
             <span>SIUPAY</span>
           </div>
           <div className="footer-links">
